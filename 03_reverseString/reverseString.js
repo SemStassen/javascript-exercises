7192e8ceec90e0a4ec371281fc252138e60b74fb
SemStassen <semstassen@gmail.com>
2022-06-08 00:04:52 +0200

    finished excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = ""
+  for (let i = string.length; i >= 0; i--) {
+      let part = string.substring(i, i+1)
+      reverseString += part;
+  }  
+  return reverseString
 };
 
 // Do not edit below this line
